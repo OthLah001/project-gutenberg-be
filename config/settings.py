b2253e2e00052f765a8ecdd97663f23c6117b6f7
OthLah001 <othmane.lahrimi.work@gmail.com>
2025-03-05 14:52:40 +0000

    fix: add celery ssl config

--- config/settings.py
+++ config/settings.py
@@ -157,3 +157,11 @@ CELERY_BROKER_URL = env("REDIS_URL")
 CELERY_RESULT_BACKEND = env("REDIS_URL")
 CELERY_ACCEPT_CONTENT = ["json"]
 CELERY_TASK_SERIALIZER = "json"
+
+if env("REDIS_URL").startswith("rediss://"):
+    CELERY_BROKER_USE_SSL = {
+        "ssl_cert_reqs": "CERT_NONE",
+        "ssl_keyfile": None,
+        "ssl_certfile": None,
+        "ssl_ca_certs": None,
+    }
